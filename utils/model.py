c549a6e87618d965844b622c4c0633c5ebe3e00e
Alif Fatul <aliffatulmf@gmail.com>
2024-07-20 20:23:17 +0700

    rename `auto_save_model` to `checkpoint`
    Signed-off-by: Alif Fatul <aliffatulmf@gmail.com>

--- utils/model.py
+++ utils/model.py
@@ -1,14 +1,10 @@
 import os
 from datetime import datetime
 
-import torch
-from torch.nn.modules import Module
-from torchvision.transforms import v2
-
-from utils.path import SubDir
+from torch import save as torch_save
 
 
-def _generate_checkpoint(model, classes, transform):
+def _generate_checkpoint(model, classes, transform, **kwargs):
     return {
         'metadata': {
             'datetime': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -21,51 +17,25 @@ def _generate_checkpoint(model, classes, transform):
 
         },
         'model_state': model.state_dict(),
+        **kwargs
     }
 
 
-def auto_save_model(root: str, name: str, prefix: str, model: Module, classes: dict, transform: v2.Compose):
+def checkpoint(root, name, model, classes, transform, **kwargs):
     """
-    Automatically saves the model to a file.
+    Saves a model checkpoint including metadata, model state, and transformations.
 
     Args:
-        root (str): The root directory where the model will be saved.
-        name (str): The name of the model.
-        prefix (str): The prefix to be added to the saved file.
-        model (Module): The model to be saved.
-        num_classes (int): The number of classes in the model.
-        transform (v2.Compose): The transformation applied to the model.
+        root (str): Directory for saving the model.
+        name (str): Filename for the saved model.
+        model (torch.nn.Module): Model to save.
+        classes (dict): Class index-to-label mapping.
+        transform (torchvision.transforms.Compose): Transformations applied to inputs.
+        **kwargs: Additional metadata for the checkpoint.
 
     Returns:
-        str: The directory where the model is saved.
-
-    Raises:
-        FileNotFoundError: If the root directory does not exist.
-
-    Example:
-        >>> root = '/path/to/save/directory'
-        >>> name = 'my_model'
-        >>> prefix = 'v1'
-        >>> model = MyModel()
-        >>> num_classes = 10
-        >>> transform = transforms.Compose([transforms.ToTensor()])
-        >>> auto_save_model(root, name, prefix, model, num_classes, transform)
-        '/path/to/save/directory/v1'
-
-    This function saves the model to a file in the specified root directory. The saved file will have the name
-    '{name}.pt' and will be stored in a subdirectory of the root directory. The subdirectory name is generated
-    using the provided prefix and a unique identifier.
-
-    The model is saved using the PyTorch `torch.save` function. Before saving, a checkpoint is generated using
-    the `_generate_checkpoint` function, which includes the model, the number of classes, and the transformation
-    applied to the model.
-
-    If the root directory does not exist, a `FileNotFoundError` is raised.
-
+        str: Path to the saved model.
     """
     filename = f'{name}.pt'
-    basedir = SubDir(root).next(prefix)
-    filepath = os.path.join(basedir, filename)
-
-    torch.save(_generate_checkpoint(model, classes, transform), filepath)
-    return basedir
+    filepath = os.path.join(root, filename)
+    torch_save(_generate_checkpoint(model, classes, transform, **kwargs), filepath)
