aa7539d636fe93921012444a28600b0f9be6b4c6
Alif Fatul <aliffatulmf@gmail.com>
2024-07-22 12:56:13 +0700

    merge checkpoint and metadata generator

--- utils/model.py
+++ utils/model.py
@@ -1,41 +1,50 @@
 import os
 from datetime import datetime
+from typing import Any, Dict
 
-from torch import save as torch_save
+import torch
+import torchvision
 
 
-def _generate_checkpoint(model, classes, transform, **kwargs):
-    return {
+def checkpoint(
+    root: str,
+    name: str,
+    model: torch.nn.Module,
+    classes: Dict[int, str],
+    transform: torchvision.transforms.Compose,
+    **kwargs: Dict[str, Any]
+) -> None:
+    """
+    Saves a model checkpoint including metadata, model state, and transformations.
+
+    Args:
+        root (str): Directory to save the model to.
+        name (str): Filename for the saved model.
+        model (torch.nn.Module): Model to save.
+        classes (dict): Class index-to-label mapping.
+        transform (torchvision.transforms.Compose): Transformations applied to inputs.
+        kwargs: Additional metadata for the checkpoint.
+
+    Returns:
+        str: Path to the saved model.
+
+    Example:
+        >>> checkpoint('runs/train/weight', 'best', model, classes, transform)
+        'runs/train/weight/best.pt'
+    """
+    metadata = {
         'metadata': {
-            'datetime': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'datetime': datetime.now().isoformat(),
             'model': model.__class__.__name__,
             'format': '.pt',
         },
         'model_info': {
             'classes': classes,
             'transform': transform,
-
         },
-        'model_state': model.state_dict(),
-        **kwargs
+        **kwargs,
     }
 
-
-def checkpoint(root, name, model, classes, transform, **kwargs):
-    """
-    Saves a model checkpoint including metadata, model state, and transformations.
-
-    Args:
-        root (str): Directory for saving the model.
-        name (str): Filename for the saved model.
-        model (torch.nn.Module): Model to save.
-        classes (dict): Class index-to-label mapping.
-        transform (torchvision.transforms.Compose): Transformations applied to inputs.
-        **kwargs: Additional metadata for the checkpoint.
-
-    Returns:
-        str: Path to the saved model.
-    """
     filename = f'{name}.pt'
     filepath = os.path.join(root, filename)
-    torch_save(_generate_checkpoint(model, classes, transform, **kwargs), filepath)
+    torch.save(metadata, filepath)
